d15c87db4043b6f4084fedfbc2bdcff71439a613
hoangvt <hoang@sigma-solutions.eu>
2022-05-27 17:59:27 +0700

    Fixbug

--- cracking_coding_interview/chapter10/searchandsorting.py
+++ cracking_coding_interview/chapter10/searchandsorting.py
@@ -32,12 +32,63 @@ def mergeSortedArrays(sortedA,sortedB):
             current_sorted_element_number += 1
     return sortedMergedArray        
 
+def is_anagram(s1,s2):
+    pass
+
+def get_keys(s,key_to_pos):
+    result = -1
+    for key in key_to_pos:
+        if is_anagram(s,key):
+            result = key_to_pos[key]
+            break
+    return result    
+
+def group_anagrams(arrays):
+    marked_array = [-1 for i in range(0,len(arrays))]
+    key_to_pos = dict()
+    for i in range(0,len(arrays)):
+        pos = get_keys(arrays[i],key_to_pos)
+        if pos < 0:
+            key_to_pos[arrays[i]] = i
+            marked_array[i] = 1
+        else:
+            marked_array[pos] +=1
+            marked_array[i] = pos 
 
 
-sortedA = [1,3,5,5]
-sortedB =  [2,4,6]
+#  abc = 5 6 7 1 2 3 4| 5 6 7
+               
+#        0 1 2 3 4 5 6  7 8 9
 
-mergedArray = mergeSortedArrays(sortedA,sortedB)
 
-for i in mergedArray:
-    print(i)
+def get_index_of_min_item(array):
+    if len(array)  <=0: return -1
+    if len(array) == 1 : return 0
+    min_item_index = 0    
+    for i in range(1,len(array)):
+        if array[i] < array[i-1]:
+            min_item_index = i
+            break
+    return min_item_index        
+def search_rotated_array(array,value):
+    min_item_index = get_index_of_min_item(array)
+    index = binary_search_array(min_item_index,len(array)+ min_item_index -1,array,value)
+    return index
+
+def binary_search_array(start,end,array,value):
+    middle = (int((start + end)/2) + 1) % len(array)
+    if start > end: 
+        return -1
+
+    if value == array[middle]:
+        return middle
+    if value < array[middle]:
+        return binary_search_array(start,middle-1,array,value)
+    else:  
+        return binary_search_array(middle+1,end,array,value)       
+
+
+
+rotatedSortedArray = [5, 6, 7, 1, 2, 3, 4]
+index = search_rotated_array(rotatedSortedArray,3)
+print(index)
